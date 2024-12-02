b70fa6e1bad9e17ce2ec7302da0dadce28a27491
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-12-02 18:02:24 +0900

    update static data for new entropy

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,12 +5,12 @@ import { Format, createMaterial, sourceAsPrimary } from "../src/index.js";
 
 test("Format.Hex", () => {
   const namespace = createMaterial("foo").formatAs(Format.Hex);
-  expect(namespace).toBe("#bc99fd");
+  expect(namespace).toBe("#1db7d6");
 });
 
 test("Format.Hct", () => {
   const namespace = createMaterial("foo").formatAs(Format.Hct);
-  expect(namespace.toInt()).toBe(0xffbc99fd);
+  expect(namespace.toInt()).toBe(0xff1db7d6);
 });
 
 test("Format.Scheme", () => {
@@ -20,18 +20,18 @@ test("Format.Scheme", () => {
   );
   const namespace = createMaterial("foo").formatAs(SchemeVibrantNamespace);
 
-  expect(hexFromArgb(namespace.primaryPaletteKeyColor)).toBe("#bc99fd");
-  expect(hexFromArgb(namespace.secondaryPaletteKeyColor)).toBe("#856f92");
-  expect(hexFromArgb(namespace.tertiaryPaletteKeyColor)).toBe("#936995");
-  expect(hexFromArgb(namespace.neutralPaletteKeyColor)).toBe("#7b7582");
-  expect(hexFromArgb(namespace.neutralVariantPaletteKeyColor)).toBe("#7b7484");
+  expect(hexFromArgb(namespace.primaryPaletteKeyColor)).toBe("#1db7d6");
+  expect(hexFromArgb(namespace.secondaryPaletteKeyColor)).toBe("#5a7b90");
+  expect(hexFromArgb(namespace.tertiaryPaletteKeyColor)).toBe("#577aa2");
+  expect(hexFromArgb(namespace.neutralPaletteKeyColor)).toBe("#6d797d");
+  expect(hexFromArgb(namespace.neutralVariantPaletteKeyColor)).toBe("#6a7a7f");
 });
 
 test("Format.Custom", () => {
   const namespace = createMaterial("foo").formatAs(
-    Format.Custom((namespace) => namespace.toInt()),
+    Format.Custom((namespace) => namespace.toInt().toString(16)),
   );
-  expect(namespace).toBe(0xffbc99fd);
+  expect(namespace).toBe("ff1db7d6");
 });
 
 test("Format.Chalk", () => {
@@ -43,10 +43,13 @@ test("subMaterial", () => {
   const rootMaterial = createMaterial("foo");
   const namespace = rootMaterial.subMaterial("bar");
 
-  const num = namespace.formatAs(Format.Hct).toInt();
-  expect(num).toBe(0xff65abff);
+  const num = namespace.formatAs(Format.Hct).toInt().toString(16);
+  expect(num).toBe("ff00c354");
 
   // make sure the array notation gives us the same result
-  const num2 = createMaterial(["foo", "bar"]).formatAs(Format.Hct).toInt();
+  const num2 = createMaterial(["foo", "bar"])
+    .formatAs(Format.Hct)
+    .toInt()
+    .toString(16);
   expect(num2).toBe(num);
 });
